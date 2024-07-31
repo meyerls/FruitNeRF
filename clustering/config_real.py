8a702156093410900f70465011d898a6ea1626aa
Lukas Meyer <lukas.meyer@fau.de>
2024-07-31 13:32:49 +0200

    configs

---
+++ clustering/config_real.py
@@ -0,0 +1,249 @@
+# Baum 01
+
+Baum_01_SAM = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_01/sam/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 120,
+    "remove_outliers_radius": 0.015,
+    "down_sample": 0.001,
+    "eps": 0.02,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    "template_path": './clustering/apple_template.ply',
+    'apple_template_size': 1,
+    'gt_cluster': None,
+    'gt_count': 179,
+}
+
+Baum_01_SAM_Big = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_01/sam_big/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 120,
+    "remove_outliers_radius": 0.015,
+    "down_sample": 0.001,
+    "eps": 0.02,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    "template_path": './clustering/apple_template.ply',
+    'apple_template_size': 1,
+    'gt_cluster': None,
+    'gt_count': 179,
+}
+
+Baum_01_unet = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_01/unet/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 120,
+    "remove_outliers_radius": 0.015,
+    "down_sample": 0.001,
+    "eps": 0.02,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    "template_path": './clustering/apple_template.ply',
+    'apple_template_size': 1,
+    'gt_cluster': None,
+    'gt_count': 179,
+}
+
+Baum_01_unet_Big = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_01/unet_big/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 120,
+    "remove_outliers_radius": 0.015,
+    "down_sample": 0.001,
+    "eps": 0.02,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    "template_path": './clustering/apple_template.ply',
+    'apple_template_size': 1,
+    'gt_cluster': None,
+    'gt_count': 179,
+}
+
+# Baum 02
+
+Baum_02_SAM = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_02/sam/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 65,
+    "remove_outliers_radius": 0.025,
+    "down_sample": 0.001,
+    "eps": 0.03,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.1,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.9,
+    'gt_cluster': None,
+    'gt_count': 113,
+}
+
+Baum_02_SAM_Big = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_02/sam_big/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 70,
+    "remove_outliers_radius": 0.025,
+    "down_sample": 0.001,
+    "eps": 0.03,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.1,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.9,
+    'gt_cluster': None,
+    'gt_count': 113,
+}
+
+Baum_02_unet = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_02/unet/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 50,
+    "remove_outliers_radius": 0.025,
+    "down_sample": 0.001,
+    "eps": 0.03,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.1,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.9,
+    'gt_cluster': None,
+    'gt_count': 113,
+}
+
+Baum_02_unet_Big = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_02/unet_big/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 50,
+    "remove_outliers_radius": 0.025,
+    "down_sample": 0.001,
+    "eps": 0.03,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.1,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.9,
+    'gt_cluster': None,
+    'gt_count': 113,
+}
+
+# Baum 03
+
+Baum_03_unet = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_03/unet/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 100,
+    "remove_outliers_radius": 0.027,
+    "down_sample": 0.001,
+    "eps": 0.025,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.8,
+    'gt_cluster': None,
+    'gt_count': 291,
+}
+
+Baum_03_unet_Big = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_03/unet_big/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 100,
+    "remove_outliers_radius": 0.027,
+    "down_sample": 0.001,
+    "eps": 0.025,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.8,
+    'gt_cluster': None,
+    'gt_count': 291,
+}
+
+Baum_03_SAM = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_03/sam/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 100,
+    "remove_outliers_radius": 0.027,
+    "down_sample": 0.001,
+    "eps": 0.025,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.8,
+    'gt_cluster': None,
+    'gt_count': 291,
+}
+
+Baum_03_SAM_Big = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/tree_03/sam_big/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 100,
+    "remove_outliers_radius": 0.027,
+    "down_sample": 0.001,
+    "eps": 0.025,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.8,
+    'gt_cluster': None,
+    'gt_count': 291,
+}
+
+
+# Fuji
+
+Fuji_unet = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/fuji/unet/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 75,
+    "remove_outliers_radius": 0.025,
+    "down_sample": 0.001,
+    "eps": 0.025,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.2,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.1,
+    "gt_cluster": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/fuji/data/lineset_aligned.ply",
+    "gt_count": 1455
+}
+
+Fuji_unet_big = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/fuji/unet_big/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 75,
+    "remove_outliers_radius": 0.025,
+    "down_sample": 0.001,
+    "eps": 0.025,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.2,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.1,
+    "gt_cluster": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/fuji/data/lineset_aligned.ply",
+    "gt_count": 1455
+}
+
+Fuji_sam = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/fuji/sam/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 50,
+    "remove_outliers_radius": 0.03,
+    "down_sample": 0.001,
+    "eps": 0.025,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.2,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.1,
+    "gt_cluster": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/fuji/data/lineset_aligned.ply",
+    "gt_count": 1455
+
+}
+
+Fuji_sam_big = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/fuji/sam_big/semantic_colormap_cropped.ply",
+    "remove_outliers_nb_points": 50,
+    "remove_outliers_radius": 0.03,
+    "down_sample": 0.001,
+    "eps": 0.025,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.2,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 1.1,
+    "gt_cluster": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/fuji/data/lineset_aligned.ply",
+    "gt_count": 1455
+}
---
+++ clustering/config_synthetic.py
@@ -0,0 +1,185 @@
+# 01: Apple
+Apple_GT_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/01_apple/gt/semantic_colormap.ply",
+    "remove_outliers_nb_points": 200,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 0.7,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/01_apple/fruits.obj",
+    "gt_count": 283
+}
+
+Apple_SAM_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/01_apple/sam/semantic_colormap.ply",
+    "remove_outliers_nb_points": 200,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "cluster_merge_distance": 0.04,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 0.7,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/01_apple/fruits.obj",
+    "gt_count": 283
+}
+
+# 02: Pear
+Pear_GT_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/02_pear/gt/semantic_colormap.ply",
+    "remove_outliers_nb_points": 250,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    "cluster_merge_distance": 0.03,
+    "template_path": './clustering/pear_template.ply',
+    'apple_template_size': 1.1,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/02_pear/fruits.obj",
+    "gt_count": 250
+}
+
+Pear_SAM_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/02_pear/sam/semantic_colormap.ply",
+    "remove_outliers_nb_points": 250,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    "cluster_merge_distance": 0.03,
+    "template_path": './clustering/pear_template.ply',
+    'apple_template_size': 1.1,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/02_pear/fruits.obj",
+    "gt_count": 250
+}
+
+# 03: Plum
+Plum_GT_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/03_plum/gt/semantic_colormap.ply",
+    "remove_outliers_nb_points": 250,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "cluster_merge_distance": 0.03,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 0.35,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/03_plum/fruits.obj",
+    "gt_count": 781
+}
+
+Plum_SAM_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/03_plum/sam/semantic_colormap.ply",
+    "remove_outliers_nb_points": 250,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "cluster_merge_distance": 0.03,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 0.35,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/03_plum/fruits.obj",
+    "gt_count": 781
+}
+
+# 05: Lemon
+Lemon_GT_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/05_lemon/gt/semantic_colormap.ply",
+    "remove_outliers_nb_points": 200,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    "cluster_merge_distance": 0.06,
+    "template_path": './clustering/lemon_template.ply',
+    'apple_template_size': 1.1,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/05_lemon/fruits.obj",
+    "gt_count": 326
+}
+
+Lemon_SAM_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/05_lemon/sam/semantic_colormap.ply",
+    "remove_outliers_nb_points": 200,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "minimum_size_factor": 0.3,
+    "min_samples": 100,
+    "cluster_merge_distance": 0.06,
+    "template_path": './clustering/lemon_template.ply',
+    'apple_template_size': 1.1,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/05_lemon/fruits.obj",
+    "gt_count": 326
+}
+
+# 07: Pear
+Peach_GT_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/07_peach/gt/semantic_colormap.ply",
+    "remove_outliers_nb_points": 250,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "minimum_size_factor": 0.3,
+    "min_samples": 70,
+    "cluster_merge_distance": 0.03,
+    "template_path": './clustering/peach_template.ply',
+    'apple_template_size': 1.2,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/07_peach/fruits.obj",
+    "gt_count": 152
+}
+
+Peach_SAM_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/07_peach/sam/semantic_colormap.ply",
+    "remove_outliers_nb_points": 250,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "minimum_size_factor": 0.3,
+    "min_samples": 70,
+    "cluster_merge_distance": 0.03,
+    "template_path": './clustering/peach_template.ply',
+    'apple_template_size': 1.2,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/07_peach/fruits.obj",
+    "gt_count": 152
+}
+
+# 08: Mango
+Mango_GT_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/FruitNeRF/data/rendering/08_mango_tree_1024x1024_#300/fruit_nerf/semantic_colormap.ply",
+    "remove_outliers_nb_points": 250,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "cluster_merge_distance": 0.01,
+    "minimum_size_factor": 0.3,
+    "min_samples": 70,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 0.3,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/08_mango/fruits.obj",
+    "gt_count": 1150
+}
+
+Mango_SAM_1024x1024_300 = {
+    "path": "/home/se86kimy/Dropbox/07_data/FruitNeRF/data/rendering/08_mango_tree_1024x1024_#300_SAM/fruit_nerf/semantic_colormap.ply",
+    "remove_outliers_nb_points": 250,
+    "remove_outliers_radius": 0.01,
+    "down_sample": 0.001,
+    "eps": 0.01,
+    "cluster_merge_distance": 0.01,
+    "minimum_size_factor": 0.3,
+    "min_samples": 70,
+    'template_path': './clustering/apple_template.ply',
+    'apple_template_size': 0.3,
+    'gt_cluster': "/home/se86kimy/Dropbox/07_data/For5G/Apple_24_08_23/eval/08_mango/fruits.obj",
+    "gt_count": 1150
+}
